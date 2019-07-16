3f4c8b70163127fcdbc4a64df60083d764313f77
gongfranksh <gongfranksh@qq.com>
2019-07-16 11:27:24 +0800

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,12 @@
+DB_USER = 'root'
+DB_PASSWORD = '123456'
+DB_HOST = '192.168.168.169'
+DB_DB = 'flask-pyjwt-auth'
+
+DEBUG = True
+PORT = 3333
+HOST = "192.168.81.136"
+SECRET_KEY = "my blog"
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'mysql://' + DB_USER + ':' + DB_PASSWORD + '@' + DB_HOST + '/' + DB_DB
